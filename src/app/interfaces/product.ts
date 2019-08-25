cef3aa3b530ceb8c743859b43f2979a2aff3e0dc
Rafael Thomazzi <rafael.thomazzi@fatec.sp.gov.br>
2019-08-25 18:41:49 -0300

    CRUD WITH CLOUD FIRESTORE

---
+++ src/app/interfaces/product.ts
@@ -0,0 +1,13 @@
+export interface Product {
+    id?: string;
+    name?: string;
+    description?: string;
+    picture?: string;
+    price?: string;
+    categoria?: string;
+    vegetariano?: boolean;
+    diet?: boolean;
+    contemAlcool?: string;
+    createdAt?: number;    
+}
+
